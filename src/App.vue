ea4a5d910dc99e1c1b197f2dbb13c22ec13eb131
steven <steven.yjj1995@gmail.com>
2021-04-18 17:40:03 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Yjj/>
+</template>
+
+<script>
+import Yjj from './components/Yjj.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Yjj
+  }
+}
+</script>
