133939fb5dca982eeb96b1e9e485799541d8e50e
steven <steven.yjj1995@gmail.com>
2021-04-19 17:40:27 +0800

    实现Tab切换

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,23 +1,76 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index" />
+  <div class="jj-tabs">
+    <div class="jj-tabs-nav">
+      <div class="jj-tabs-nav-item"
+           @click="select(t)"
+           :class="{selected:t===selected}"
+           v-for="(t,index) in titles" :key="index">{{ t }}
+      </div>
+    </div>
+    <div class="jj-tabs-content">
+      <component class="jj-tabs-content-item" :is="current" :key="current.props.title"/>
+    </div>
   </div>
 </template>
 <script lang="ts">
-import Tab from './Tab.vue'
+import Tab from './Tab.vue';
+import {computed} from 'vue';
+
 export default {
-  setup(props,context){
-    const defaults =context.slots.default()
-    defaults.forEach((tag)=>{
-      if(tag.type !== Tab){
-        throw new Error('Tabs的子标签必须是Tab')
+  props: {
+    selected: {
+      type: String
+    }
+  },
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs的子标签必须是Tab');
       }
-    })
-    const titles =defaults.map((tag)=>{
+    });
+    const current = computed(() => {
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
+    });
+    const titles = defaults.map((tag) => {
       return tag.props.title;
-    })
-    return {defaults,titles}
+    });
+    const select = (title: string) => {
+      context.emit('update:selected', title);
+    };
+    return {defaults, titles, current, select};
+  }
+};
+</script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.jj-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
   }
 }
-</script>
+</style>
