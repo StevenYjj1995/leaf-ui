6e43fec229ea74552423dbf60a534dcb0e645f81
steven <steven.yjj1995@gmail.com>
2021-04-19 14:56:22 +0800

    button支持level

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -11,9 +11,9 @@
   </div>
   <h1>示例2</h1>
   <div>
-    <Button size="big">大按钮</Button>
+    <Button size="big">大大</Button>
     <Button>普通</Button>
-    <Button size="small">小按钮</Button>
+    <Button size="small">小小</Button>
   </div>
   <div>
     <Button size="big" theme="link">大按钮</Button>
@@ -24,6 +24,22 @@
     <Button size="big" theme="text">大按钮</Button>
     <Button theme="text">普通</Button>
     <Button size="small" theme="text">小按钮</Button>
+  </div>
+  <h1>示例3</h1>
+  <div>
+    <Button level="main">主要按钮</Button>
+    <Button>普通按钮</Button>
+    <Button level="danger">危险按钮</Button>
+  </div>
+  <div>
+    <Button level="main" theme="link">主要按钮</Button>
+    <Button theme="link">普通按钮</Button>
+    <Button level="danger" theme="link">危险按钮</Button>
+  </div>
+  <div>
+    <Button level="main" theme="text">主要按钮</Button>
+    <Button theme="text">普通按钮</Button>
+    <Button level="danger" theme="text">危险按钮</Button>
   </div>
 </template>
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,13 +16,18 @@ export default {
       type: String,
       default: 'normal'
     },
+    level:{
+      type:String,
+      default:'normal'
+    }
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size,level} = props;
     const classes = computed(() => {
       return {
         [`jj-theme-${theme}`]: theme,
         [`jj-size-${size}`]: size,
+        [`jj-level-${level}`]: level,
       };
     });
     return {classes};
@@ -35,6 +40,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .jj-button {
   box-sizing: border-box;
   height: $h;
@@ -49,54 +55,94 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
-
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
-
   &:hover,
   &:focus {
     color: $blue;
     border-color: $blue;
   }
-
   &:focus {
     outline: none;
   }
-
   &::-moz-focus-inner {
     border: 0;
   }
-
   &.jj-theme-link {
     border-color: transparent;
     box-shadow: none;
     color: $blue;
-
-    &:hover, &:focus {
+    &:hover,
+    &:focus {
       color: lighten($blue, 10%);
     }
   }
-
   &.jj-theme-text {
     border-color: transparent;
     box-shadow: none;
     color: inherit;
-
-    &:hover, &:focus {
-      background: darken(white, 5%);;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
     }
   }
-  &.jj-theme-button{
-    &.jj-size-big{
-      font-size: 24px;
-      height: 48px;
-      padding:0 16px ;
+  &.jj-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.jj-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.jj-theme-button {
+    &.jj-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
     }
-    &.jj-size-small{
-      font-size: 12px;
-      height: 20px;
-      padding: 0 4px;
+    &.jj-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.jj-theme-link {
+    &.jj-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.jj-theme-text {
+    &.jj-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.jj-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
     }
   }
 }
