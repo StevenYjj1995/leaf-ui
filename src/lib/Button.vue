1ef9bdfeb5d473fc5abe62244b4538745e6d568a
steven <steven.yjj1995@gmail.com>
2021-04-19 15:03:40 +0800

    button支持disabled

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="jj-button" :class="classes">
+  <button class="jj-button" :class="classes" :disabled="disabled">
     <slot/>
   </button>
 </template>
@@ -16,13 +16,17 @@ export default {
       type: String,
       default: 'normal'
     },
-    level:{
-      type:String,
-      default:'normal'
+    level: {
+      type: String,
+      default: 'normal'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
     }
   },
   setup(props) {
-    const {theme, size,level} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
       return {
         [`jj-theme-${theme}`]: theme,
@@ -41,6 +45,7 @@ $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
 $red: red;
+$grey: grey;
 .jj-button {
   box-sizing: border-box;
   height: $h;
@@ -56,59 +61,72 @@ $red: red;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
   transition: background 250ms;
+
   & + & {
     margin-left: 8px;
   }
+
   &:hover,
   &:focus {
     color: $blue;
     border-color: $blue;
   }
+
   &:focus {
     outline: none;
   }
+
   &::-moz-focus-inner {
     border: 0;
   }
+
   &.jj-theme-link {
     border-color: transparent;
     box-shadow: none;
     color: $blue;
+
     &:hover,
     &:focus {
       color: lighten($blue, 10%);
     }
   }
+
   &.jj-theme-text {
     border-color: transparent;
     box-shadow: none;
     color: inherit;
+
     &:hover,
     &:focus {
       background: darken(white, 5%);
     }
   }
+
   &.jj-size-big {
     font-size: 24px;
     height: 48px;
     padding: 0 16px;
   }
+
   &.jj-size-small {
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
   }
+
   &.jj-theme-button {
     &.jj-level-main {
       background: $blue;
       color: white;
       border-color: $blue;
+
       &:hover,
       &:focus {
         background: darken($blue, 10%);
         border-color: darken($blue, 10%);
       }
     }
+
     &.jj-level-danger {
       background: $red;
       border-color: $red;
@@ -120,29 +138,50 @@ $red: red;
       }
     }
   }
+
   &.jj-theme-link {
     &.jj-level-danger {
       color: $red;
+
       &:hover,
       &:focus {
         color: darken($red, 10%);
       }
     }
   }
+
   &.jj-theme-text {
     &.jj-level-main {
       color: $blue;
+
       &:hover,
       &:focus {
         color: darken($blue, 10%);
       }
     }
+
     &.jj-level-danger {
       color: $red;
+
       &:hover,
       &:focus {
         color: darken($red, 10%);
       }
+    }
+  }
+  &.jj-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.jj-theme-link, &.jj-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
     }
   }
 }
