b7776721c9e65e044f7470560a75ab0ac47ad886
steven <steven.yjj1995@gmail.com>
2021-04-19 13:51:15 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,12 +3,20 @@
     Button 示例
   </div>
   <div>
-    <Button>你好</Button>
+    <Button @click="onClick"
+    @focus="onClick"
+    @mouseover="onClick">你好</Button>
   </div>
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
-  components:{Button}
+  components:{Button},
+  setup(){
+    const onClick = () =>{
+      console.log('hi');
+    }
+    return {onClick}
+  }
 }
 </script>
